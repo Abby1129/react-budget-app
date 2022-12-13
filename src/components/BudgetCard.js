a9ad4f485fe447883a4286b33d3cee65f2b7934d
Abby F <abby_faigao@yahoo.com>
2022-12-13 11:22:58 -0800

    add progress bar func in budget card component

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -9,9 +9,8 @@ export default function BudgetCard(name, amount, max) {
           <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
             <div className="me-2">{name}</div>
             <div className="d-flex align-items-baseline">
-              {currencyFormatter.format(amount)}{" "}
+              {currencyFormatter.format(amount)}
               <span className="text-muted fs-6 ms-1">
-                {" "}
                 /{currencyFormatter.format(max)}
               </span>
             </div>
@@ -24,4 +23,11 @@ export default function BudgetCard(name, amount, max) {
       </Card>
     </div>
   );
+}
+
+function getProgressBarVariant(amount, max) {
+  const ratio = amount / max;
+  if (ratio < 0.5) return "primary";
+  if (ratio < 0.75) return "warning";
+  return "danger";
 }
