d71b9607911f451ffbb504fe85cafab52ed56c1d
Abby F <abby_faigao@yahoo.com>
2022-12-17 23:14:53 -0800

    usecontext hook

---
+++ src/contexts/BudgetContext.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+
+const BudgetsContext = React.createContext();
+
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized";
+
+export function useBudgets() {
+  return useContext(BudgetsContext);
+}
+
+export const BudgetsProvider = ({ children }) => {
+  return (
+    <BudgetsContext.Provider
+      value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteBudget,
+        deleteExpense,
+      }}
+    >
+      {children}
+    </BudgetsContext.Provider>
+  );
+};
