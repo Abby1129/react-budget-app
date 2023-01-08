95443fc1decbc7dcf16d3ea7e276bdbcb8049fcd
Abby F <abby_faigao@yahoo.com>
2023-01-08 15:54:30 -0800

    create add expense func

--- src/contexts/BudgetContext.js
+++ src/contexts/BudgetContext.js
@@ -16,6 +16,12 @@ export const BudgetsProvider = ({ children }) => {
     return expenses.filter((expense) => expense.budgetId === budgetId);
   }
 
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+
   return (
     <BudgetsContext.Provider
       value={{
